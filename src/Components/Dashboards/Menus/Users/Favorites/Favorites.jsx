bee84a40d8479ada0ebefc84eec9331d9e92c14e
MH-liton-360 <liton41126nk@gmail.com>
2026-04-16 21:04:18 +0600

    favorites

--- src/Components/Dashboards/Menus/Users/Favorites/Favorites.jsx
+++ src/Components/Dashboards/Menus/Users/Favorites/Favorites.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import SectionTitle from "../../../../Home/SectionTitle/SectionTitle";
 
 const Favorites = () => {
     const [favorites, setFavorites] = useState([]);
@@ -12,22 +13,27 @@ const Favorites = () => {
     }, []);
 
     return (
-        <section className="p-6">
-            <h1 className="text-xl font-bold mb-4">My Favorites</h1>
+        <section>
+            <SectionTitle
+                heading={"Favorites"}
+            ></SectionTitle>
+            <section className="p-6">
+                <h1 className="text-xl font-bold mb-4">My Favorites</h1>
 
-            {favorites.length === 0 ? (
-                <p>No favorites yet</p>
-            ) : (
-                <div className="grid md:grid-cols-2 gap-4">
-                    {favorites.map(fav => (
-                        <div key={fav._id} className="bg-amber-100 p-4 rounded">
-                            <h3 className="font-bold">{fav.biodata?.name}</h3>
-                            <p>{fav.biodata?.profession}</p>
-                            <p>{fav.biodata?.district}</p>
-                        </div>
-                    ))}
-                </div>
-            )}
+                {favorites.length === 0 ? (
+                    <p>No favorites yet</p>
+                ) : (
+                    <div className="grid md:grid-cols-2 gap-4">
+                        {favorites.map(fav => (
+                            <div key={fav._id} className="bg-amber-100 p-4 rounded">
+                                <h3 className="font-bold">{fav.biodata?.name}</h3>
+                                <p>{fav.biodata?.profession}</p>
+                                <p>{fav.biodata?.district}</p>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </section>
         </section>
     );
 };
