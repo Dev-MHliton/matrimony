86766804aa360c3544a4cdae64fb6705165c2724
MH-liton-360 <liton41126nk@gmail.com>
2026-04-19 23:24:44 +0600

    Error

--- src/Components/Error/Errors/Error.jsx
+++ src/Components/Error/Errors/Error.jsx
@@ -3,19 +3,23 @@ import error from '../../../assets/Error/404.mp4'
 
 const Error = () => {
     return (
-        <section>
+        <section className="h-screen w-full overflow-hidden">
             <title>
                 404 - Page no found Matrimony
             </title>
 
-            <video autoPlay loop muted className="h-full">
+            <video
+                autoPlay
+                loop
+                muted
+                className="h-full w-full object-cover"
+            >
                 <source src={error} type="video/mp4" />
             </video>
 
-
             {/* <Link
                 to="/"
-                className="inline-block bg-green-600 hover:bg-green-700 text-white font-medium px-6 py-3 rounded-lg transition duration-300 shadow-lg"
+                className="absolute bottom-10 left-1/2 -translate-x-1/2 bg-green-600 hover:bg-green-700 text-white font-medium px-6 py-3 rounded-lg transition duration-300 shadow-lg"
             >
                 Back to Home
             </Link> */}
